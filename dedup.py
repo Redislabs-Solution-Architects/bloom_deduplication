4c08f786f8402e274eb827d881ec2ecffbcc3299
Chris Mague <github@mague.com>
2021-01-05 07:06:28 -0800

    fix bug in dedup gear

--- dedup.py
+++ dedup.py
@@ -3,7 +3,7 @@ def runIt(x):
     name = x['value']['Name']
     #execute('TS.INCRBY', 's-unfiltered', 1, 'TIMESTAMP', int(int(x['id'].split('-')[0])/1000))
     execute('TS.INCRBY', 's-unfiltered', 1, 'TIMESTAMP', '*')
-    j = execute("BF.ADD", val, "DEDUP")
+    j = execute("BF.ADD", "DEDUP", val)
     if j > 0:
         #execute('TS.INCRBY', 's-filtered', 1, 'TIMESTAMP', int(int(x['id'].split('-')[0])/1000))
         execute('TS.INCRBY', 's-filtered', 1, 'TIMESTAMP', '*')
